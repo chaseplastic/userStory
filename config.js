3a5718975b7fc6b18a009ae0c9935eaac0b23e08
Dave Kendall <david.c.kendall@gmail.com>
2015-11-29 21:54:44 -0500

    Commit from Dropbox
    I think this is necessary. Not sure because I just drag and dropped.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+  "database": "mongodb://chaseplastic4atd:Invalidated144@ds053944.mongolab.com:53944/userstory",
+  "port": process.env.PORT || 3000,
+  "secretKey": "YourSecretKey"
+}
